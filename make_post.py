c5c2184c571744c2afaa9b39736d6be4c7d1af2a
idekworks <99700493+idekworks@users.noreply.github.com>
2022-07-11 19:28:47 +0900

    post

--- make_post.py
+++ make_post.py
@@ -22,7 +22,7 @@ with open(file_name, 'x') as f:
     sl = ['# atcoder','\n\n' '# kaggle', '\n\n' '# 統計', '\n\n',
             '# python algo', '\n\n', '# dev app', '\n\n', '# other' '\n\n']
     f.writelines(sl) 
-    f.write('***\n') 
+    f.write('***\n\n') 
     f.write(f'[<prev]({prev_name}) | [next>]({next_name})\n\n')
 
 
